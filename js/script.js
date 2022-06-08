c417b1d20675019bf7090509a58f804c86d62263
tyseer <tyseerhe@gmail.com>
2022-06-08 18:35:15 +0300

    changed some stuff

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let sum = 0;
 let arr = [];
-function Students() {
-
+let btn = document.getElementById("send");
+btn.addEventListener("click", () => {
     let elements = [];
     elements[0] = document.getElementById("name");
     elements[1] = document.getElementById("score");
@@ -10,57 +10,59 @@ function Students() {
     elements[4] = document.getElementById("bodyy");
 
 
+
     if (elements[0].value == "" || elements[0].value == null) {
         elements[0].style.borderColor = 'red';
         alert("please enter your name");
     }
-    else if (elements[1].value == "" || elements[1].value == null) {
+    if (elements[1].value == "" || elements[1].value == null) {
         elements[1].style.borderColor = 'red';
         alert("please enter your score");
     }
     else if (isNaN(elements[1].value) || elements[1].value == null) {
+        elements[1].style.borderColor = 'red';
         alert("please enter a number for score");
     }
     else if (elements[1].value < 0 || elements[1].value > 100) {
+        elements[1].style.borderColor = 'red';
         alert("please make sure that your score is between 0-100");
     }
 
     else {
 
 
-       let ele1 = document.createElement("label");
-       let ele2 = document.createElement("label");
-       let ele3=document.createElement("img");
-        ele3.className="str";
-       let ele4= document.createElement("label");
+        let ele1 = document.createElement("label");
+        let ele2 = document.createElement("label");
+        let ele3 = document.createElement("img");
+        ele3.className = "str";
+        let ele4 = document.createElement("label");
 
-       let ele5 = document.createElement("td");
-       let ele6 = document.createElement("td");
-       let ele7 = document.createElement("td");
-       let ele8 = document.createElement("td");
+        let ele5 = document.createElement("td");
+        let ele6 = document.createElement("td");
+        let ele7 = document.createElement("td");
+        let ele8 = document.createElement("td");
 
-       let ele9 = document.createElement("tr");
+        let ele9 = document.createElement("tr");
 
-       ele1.textContent = elements[0].value;
-       ele2.textContent = elements[1].value;
-       ele3.src=elements[3].value;
-       ele4.textContent = elements[2].value
+        ele1.textContent = elements[0].value;
+        ele2.textContent = elements[1].value;
+        ele3.src = elements[3].value;
+        ele4.textContent = elements[2].value
+        el10=elements[4];
 
-       
-       ele5.appendChild(ele1);
-       ele6.appendChild(ele2);
-       ele7.appendChild(ele3);
-       ele8.appendChild(ele4);
-       ele9.appendChild(ele5);
-       ele9.appendChild(ele6);
-       ele9.appendChild(ele7);
-       ele9.appendChild(ele8);
+        ele5.appendChild(ele1);
+        ele6.appendChild(ele2);
+        ele7.appendChild(ele3);
+        ele8.appendChild(ele4);
+        ele9.appendChild(ele5);
+        ele9.appendChild(ele6);
+        ele9.appendChild(ele7);
+        ele9.appendChild(ele8);
+        el10.appendChild(ele9);
 
-       elements[4].appendChild(ele9);
+        var counter = el10.getElementsByTagName("tr");
 
-       var counter = elements[4].getElementsByTagName("tr");
-
-        let x1 = document.getElementById("info");
+        let x1 = document.getElementById("count");
         x1.textContent = counter.length;
 
         let x2 = document.getElementById("avg");
@@ -82,7 +84,24 @@ function Students() {
         elements[0].value = "";
         elements[1].value = "";
 
+
         localStorage.setItem('data', JSON.stringify(arr));
 
     }
-}
+
+    if (elements[2].value == "" || elements[2].value == null) {
+        elements[2].style.borderColor = 'red';
+        alert("update the time please!");
+    }
+
+    if (elements[3].value == "" || elements[3].value == null) {
+        elements[3].style.borderColor = 'red';
+        alert("please put the img url here!");
+    }
+
+    
+
+    elements[2].value = "";
+    elements[3].value = "";
+
+});
